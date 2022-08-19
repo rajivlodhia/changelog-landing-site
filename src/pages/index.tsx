3ea0ca211cf1473d031630db03c03fcf2146a7af
Connor Dinnadge <cdinnadge97@gmail.com>
2022-08-20 00:24:43 +0100

    setup base project
    setup styled components, react icons in a nextjs typescript project

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Changelog hosting api | Chxngelog</title>
+        <meta name="description" content="Changelogs" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        <h1>
+          Chxngelog
+        </h1>
+      </div>
+    </>
+  );
+};
+
+export default Home;
